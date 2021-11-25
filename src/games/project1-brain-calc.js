a9cb0d9de190a77ba7ce80e7a9592cafc3dcda33
sergei mishin <mishins.s@yandex.ru>
2021-11-25 20:34:55 +0300

    correction brain calc

--- src/games/project1-brain-calc.js
+++ src/games/project1-brain-calc.js
@@ -1,36 +1,32 @@
 import mainGameFunction from '../index.js';
 
-// Функция возвращает случайное число
-const getRandomInt = (max) => {
-  const random = Math.floor(Math.random() * max);
-  return random;
-};
+const getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const isRules = 'What is the result of the expression?';
-const arrOperator = ['*', '+', '-'];
 
-// Функция возвращает случайного оператора
-let result = '';
-const getRandomArr = () => {
-  for (let i = 0; i < arrOperator.length; i += 1) {
-    const randomArr = getRandomInt(3);
-    result = arrOperator[randomArr];
+const getRandomSign = (numberFirst, numberSecond, randomSign) => {
+  switch (randomSign) {
+    case '+': return numberFirst + numberSecond;
+    case '-': return numberFirst - numberSecond;
+    case '*': return numberFirst * numberSecond;
+    default:
+      throw new Error('Can`t calculate expression');
   }
-  return result;
 };
 
-// Функция возвращает результат операции со случайным оператором
-const getCalc = () => {
-  const randomNum1 = getRandomInt(100);
-  const randomNum2 = getRandomInt(100);
-  const randomOperator = getRandomArr(arrOperator);
-  const question = (randomNum1 + randomOperator + randomNum2);
-  const calc = (eval(randomNum1 + randomOperator + randomNum2));
-  return [question, String(calc)];
+const generateRound = () => {
+  const randomSignArray = ['+', '-', '*'];
+  const randomNumber = getRandom(0, randomSignArray.length - 1);
+  const numberFirst = getRandom(0, 100);
+  const numberSecond = getRandom(0, 100);
+  const sign = randomSignArray[randomNumber];
+  const question = `${numberFirst} ${sign} ${numberSecond}`;
+  const result = getRandomSign(numberFirst, numberSecond, sign);
+  return [question, String(result)];
 };
 
 const letPlay = () => {
-  mainGameFunction(getCalc, isRules);
+  mainGameFunction(generateRound, isRules);
 };
 
 export default letPlay;
